f5b6ba30676b26c38c2dceef452cac73772986c2
maurizWeymann <s0528279@htw-berlin.de>
2022-01-14 19:40:31 +0100

    initial commit

---
+++ src/main/java/de/htwberlin/utils/DbCred.java
@@ -0,0 +1,9 @@
+package de.htwberlin.utils;
+
+public interface DbCred {
+	  final String driverClass = "oracle.jdbc.driver.OracleDriver";
+	final String url = "jdbc:oracle:thin:@oradbs03.f4.htw-berlin.de:1521:oradb1";
+	final String user = "u528279";
+	final String password = "p528279";
+	final String schema = "u528279";
+}
